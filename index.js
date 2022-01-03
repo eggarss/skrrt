79d537db89e804ee25e9aa694395df898e13b1d2
xSoulja <60967993+eggarss@users.noreply.github.com>
2022-01-03 09:48:50 +0000

    ss

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 const fetch = require("node-fetch");
 process.env.PORT || 3000;
 
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 client.login(process.env.TOKEN);
 
 client.on("ready", ()=> {
