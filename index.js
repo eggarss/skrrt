512eee508bd4ccc6a337acbadaf37b384fbe3263
xSoulja <60967993+eggarss@users.noreply.github.com>
2022-01-04 12:27:10 +0000

    asd

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ client.on("messageCreate", async (msg) => {
     }}
     catch(err){
         console.log(`Neatrada tadu gifu! ${err}`);
-        //msg.react('❌');
+        msg.react('❌');
     }
 });
 //GIF
