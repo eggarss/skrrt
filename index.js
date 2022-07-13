1dbf66ce5f7c7c2d70bf6d82b960f5e0936c4845
xSoulja <60967993+eggarss@users.noreply.github.com>
2022-07-13 09:58:03 +0000

    sapisās gif

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"], })
-const fetch = require("node-fetch");
+//const fetch = require("node-fetch");
 const pagination = require('discord.js-pagination');
 const snekfetch = require('snekfetch');
 process.env.PORT || 3000;
@@ -257,6 +257,7 @@ client.on("messageCreate", async (msg) => {
 
 
 //GIF
+/*
 client.on("messageCreate", async (msg) => {
   try {
     let query = msg.content.split(" ");
@@ -275,7 +276,7 @@ client.on("messageCreate", async (msg) => {
       const gifcmd = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setImage(gif.url)
-      msg.channel.send({ embeds: [gifcmd] })
+      msg.channel.send({ embeds: [gifcmd]})
 
     }
   }
@@ -284,7 +285,7 @@ client.on("messageCreate", async (msg) => {
     msg.react('❌');
   }
 });
-
+*/
 //GIPHY
 /*client.on("messageCreate", async (msg) => {
   try{
