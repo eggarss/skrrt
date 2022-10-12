28213458b0281d8ea54fcba4db888af48d4c93e0
xSoulja <60967993+eggarss@users.noreply.github.com>
2022-10-12 15:24:55 +0000

    activity

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ client.login(process.env.TOKEN);
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  //client.user.setActivity("with Dick", { type: "PLAYING" })
+  client.user.setActivity("with Dick", { type: "PLAYING" })
 })
 
 
